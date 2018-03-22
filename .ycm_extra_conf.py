826ef683ee1c0b12532d42fb231c736767369922
amar <amar.lakshya@xaviers.edu.in>
2018-03-22 18:32:07 +0530

    UPDATED: ycm config

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,5 +1,9 @@
 def FlagsForFile( filename, **kwargs ):
-    return {
-            'flags': [ '-x', 'c++','-std=c++14', '-Wall', '-Wextra', '-I/home/amar/github/julia_ros_ws/install/include', '-I/home/amar/github/julia_code/install/include', '-I/home/amar/github/julia_code/src/julia_embedded/include/'],
-            'flags': [ '-x', 'c','-std=c99', '-Wall', '-Wextra'],
-            }
+    if ".cpp" in filename:
+        return {
+                'flags': [ '-x', 'c++','-std=c++14', '-Wall', '-Wextra', '-I/home/amar/github/julia_ros_ws/install/include', '-I/home/amar/github/julia_code/install/include', '-I/home/amar/github/julia_code/src/julia_embedded/include/'],
+                }
+    else:
+        return {
+                'flags': [ '-x', 'c','-std=c99', '-Wall', '-Wextra'],
+                }
